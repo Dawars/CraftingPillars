374849bd9f183bf936e72903276e51de9f83461c
Fehér Balázs <fbalazs369@gmail.com>
2013-10-29 18:30:45 +0100

    Improved number rendering with background

--- me/dawars/CraftingPillars/renderer/RenderingHelper.java
+++ me/dawars/CraftingPillars/renderer/RenderingHelper.java
@@ -32,7 +32,7 @@ public class RenderingHelper
 			if(number > 0 && showNum)
 			{
 				glDisable(GL_LIGHTING);
-				renderFloatingText(x, y+(this.bob ? 0.5F : 0.4F), z, 0.4F, ""+number, Color.white.getRGB());
+				renderFloatingTextWithBackground(x, y+(this.bob ? 0.6F : 0.4F), z, 0.4F, ""+number, Color.white.getRGB(), new Color(0F, 0F, 0F, 0.5F));
 				glEnable(GL_LIGHTING);
 			}
 			this.doRenderItem(item, x, y, z, 0F, 0F);
@@ -51,32 +51,47 @@ public class RenderingHelper
 		}
 	}
 	
-	/*public static void renderFloatingRect(float x, float y, float z, float width, float height)
+	public static void applyFloatingRotations()
 	{
-		FontRenderer fontRenderer = RenderManager.instance.getFontRenderer();
-		glPushMatrix();
-			glTranslatef(x, y, z);
-			glRotatef(-RenderManager.instance.playerViewY, 0.0F, 1.0F, 0.0F);
-			glRotatef(RenderManager.instance.playerViewX, 1.0F, 0.0F, 0.0F);
-			glRotatef(180F, 0.0F, 0.0F, 1.0F);
-			glBegin(GL_QUADS);
-				glVertex2f(-width, height);
-				glVertex2f(-width, -height);
-				glVertex2f(width, -height);
-				glVertex2f(width, height);
-			glEnd();
-		glPopMatrix();
-	}*/
+		glRotatef(-RenderManager.instance.playerViewY, 0.0F, 1.0F, 0.0F);
+		glRotatef(RenderManager.instance.playerViewX, 1.0F, 0.0F, 0.0F);
+	}
 	
 	public static void renderFloatingText(float x, float y, float z, float scale, String text, int color)
 	{
 		FontRenderer fontRenderer = RenderManager.instance.getFontRenderer();
 		glPushMatrix();
 			glTranslatef(x, y, z);
-			glRotatef(-RenderManager.instance.playerViewY, 0.0F, 1.0F, 0.0F);
-			glRotatef(RenderManager.instance.playerViewX, 1.0F, 0.0F, 0.0F);
+			applyFloatingRotations();
 			glRotatef(180F, 0.0F, 0.0F, 1.0F);
 			glScalef(0.05F*scale, 0.05F*scale, 1F);
+			fontRenderer.drawString(text, -fontRenderer.getStringWidth(text)/2, -fontRenderer.FONT_HEIGHT/2, color);
+		glPopMatrix();
+	}
+	
+	public static void renderFloatingTextWithBackground(float x, float y, float z, float scale, String text, int color, Color bgColor)
+	{
+		FontRenderer fontRenderer = RenderManager.instance.getFontRenderer();
+		glPushMatrix();
+			glTranslatef(x, y, z);
+			applyFloatingRotations();
+			glRotatef(180F, 0.0F, 0.0F, 1.0F);
+			glScalef(0.05F*scale, 0.05F*scale, 1F);
+			
+			int h = fontRenderer.FONT_HEIGHT/2;
+			int w = fontRenderer.getStringWidth(text)/2;
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glColor4f((float)bgColor.getRed()/256F, (float)bgColor.getGreen()/256F, (float)bgColor.getBlue()/256F, (float)bgColor.getAlpha()/256F);
+			glBindTexture(GL_TEXTURE_2D, 0);
+			glBegin(GL_QUADS);
+				glVertex3f(-w-1, -h-1, 0.001F);
+				glVertex3f(-w-1, h, 0.001F);
+				glVertex3f(w, h, 0.001F);
+				glVertex3f(w, -h-1, 0.001F);
+			glEnd();
+			glDisable(GL_BLEND);
+			
 			fontRenderer.drawString(text, -fontRenderer.getStringWidth(text)/2, -fontRenderer.FONT_HEIGHT/2, color);
 		glPopMatrix();
 	}
