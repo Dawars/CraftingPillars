d1e268b68daa350a54fd54ad6adc0dea86564ad0
Balázs Fehér <fbalazs369@gmail.com>
2013-11-26 21:49:03 +0100

    Isn't tested yet

--- me/dawars/CraftingPillars/handlers/PillarEventHandler.java
+++ me/dawars/CraftingPillars/handlers/PillarEventHandler.java
@@ -1,8 +1,11 @@
 package me.dawars.CraftingPillars.handlers;
 
+import me.dawars.CraftingPillars.CraftingPillars;
 import me.dawars.CraftingPillars.properties.CalendarPlayerProps;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
 
 public class PillarEventHandler
@@ -13,6 +16,17 @@ public class PillarEventHandler
 		if(event.entity instanceof EntityPlayer && CalendarPlayerProps.get((EntityPlayer)event.entity) == null)
 		{
 			CalendarPlayerProps.register((EntityPlayer)event.entity);
+		}
+	}
+	
+	@ForgeSubscribe
+	public void onEntityJoinWorld(EntityJoinWorldEvent event)
+	{
+		if(event.entity instanceof EntityPlayer)
+		{
+			EntityPlayer player = (EntityPlayer)event.entity;
+			if(CraftingPillars.winter && !player.inventory.hasItem(CraftingPillars.itemCalendar.itemID))
+				player.inventory.addItemStackToInventory(new ItemStack(CraftingPillars.itemCalendar.itemID, 1, 0));
 		}
 	}
 }
