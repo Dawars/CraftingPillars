c317ced7dfbe008d875b3887c7a63e2b2fd416d3
Dawars <dawars00@gmail.com>
2015-01-08 22:29:50 +0100

    Update to latest source

--- me/dawars/CraftingPillars/client/gui/GuiIds.java
+++
@@ -1,7 +0,0 @@
-package me.dawars.CraftingPillars.client.gui;
-
-public class GuiIds
-{
-	public static final int ADVENT_CALENDAR = 0;
-	public static final int ELYSIUM_LORE = 1;
-}
--- me/dawars/CraftingPillars/tiles/TileEntityExtendPillar.java
+++
@@ -1,8 +0,0 @@
-package me.dawars.CraftingPillars.tiles;
-
-import net.minecraft.tileentity.TileEntity;
-
-public class TileEntityExtendPillar extends TileEntity
-{
-
-}
