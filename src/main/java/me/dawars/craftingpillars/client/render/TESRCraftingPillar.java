d1b695f50d1ba4361df66479dfcd82c4069de42a
dawars <dawars00@gmail.com>
2017-08-08 15:37:12 +0200

    CraftinPillar rendering cleanup

--- src/main/java/me/dawars/craftingpillars/client/render/TESRCraftingPillar.java
+++ src/main/java/me/dawars/craftingpillars/client/render/TESRCraftingPillar.java
@@ -10,28 +10,20 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class TESRCraftingPillar extends TileEntitySpecialRenderer<TileEntityCraftingPillar> {
-    private static final EntityItem ITEM =
-            new EntityItem(Minecraft.getMinecraft().theWorld, 0, 0, 0, new ItemStack(Items.APPLE));
     private final EntityItem entityItem = new EntityItem(Minecraft.getMinecraft().theWorld, 0, 0, 0);
 
     @Override
     public void renderTileEntityAt(TileEntityCraftingPillar te, double x, double y, double z, float partialTicks, int destroyStage) {
         super.renderTileEntityAt(te, x, y, z, partialTicks, destroyStage);
 
-        ITEM.hoverStart = 0;
-
-        Minecraft.getMinecraft().getRenderManager().doRenderEntity(ITEM, x, y, z, 0, 0, false);
-
-        entityItem.hoverStart = 0;
+        entityItem.hoverStart = 0; // crafting ingredients stay still
 
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
                 ItemStack stackInSlot = te.getStackInSlot(j * 3 + i);
                 if (stackInSlot != null && stackInSlot.stackSize > 0) {
                     entityItem.setEntityItemStack(stackInSlot);
-                    entityItem.hoverStart = 0;
 
-                    // TODO lightning not working?
                     GlStateManager.pushMatrix();
                     GlStateManager.translate(3. / 16 + x + 5. / 16 * i, y + 1 - 2. / 16, 3. / 16 + z + 5. / 16 * j);
                     GlStateManager.scale(4. / 8, 4. / 8, 4. / 8);
@@ -44,7 +36,7 @@ public class TESRCraftingPillar extends TileEntitySpecialRenderer<TileEntityCraf
 
         if (te.getResultStack() != null) {
             entityItem.setEntityItemStack(te.getResultStack());
-            entityItem.hoverStart = -te.getItemRotation(partialTicks);
+            entityItem.hoverStart = te.getItemRotation(partialTicks); // crafting result spins
 
             GlStateManager.pushMatrix();
             GlStateManager.translate(x + 0.5, y + 1.5, z + 0.5);
@@ -52,10 +44,5 @@ public class TESRCraftingPillar extends TileEntitySpecialRenderer<TileEntityCraf
                     0, 0, 0, 0, 0, false);
             GlStateManager.popMatrix();
         }
-    }
-
-    @Override
-    public boolean isGlobalRenderer(TileEntityCraftingPillar te) {
-        return false;
     }
 }
